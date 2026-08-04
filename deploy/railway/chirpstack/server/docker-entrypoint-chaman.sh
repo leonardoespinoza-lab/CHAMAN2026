fc18c2e9c4292bef389e8d28e98b1f14745007ae
Leonardo Espinoza <leonardoespinoza-lab@users.noreply.github.com>
2026-08-04 11:27:41 -0300

    Use ChirpStack 4.19 password command

--- deploy/railway/chirpstack/server/docker-entrypoint-chaman.sh
+++ deploy/railway/chirpstack/server/docker-entrypoint-chaman.sh
@@ -4,7 +4,7 @@ set -eu
 if [ -n "${CHIRPSTACK_ADMIN_PASSWORD:-}" ]; then
   umask 077
   printf '%s' "$CHIRPSTACK_ADMIN_PASSWORD" > /tmp/chirpstack-admin-password
-  /usr/bin/chirpstack --config /etc/chirpstack reset-password \
+  /usr/bin/chirpstack --config /etc/chirpstack set-password \
     --email "${CHIRPSTACK_ADMIN_EMAIL:-admin}" \
     --password-file /tmp/chirpstack-admin-password
   rm -f /tmp/chirpstack-admin-password
